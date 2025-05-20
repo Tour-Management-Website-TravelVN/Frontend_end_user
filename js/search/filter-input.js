5c980624c7fc373d01230b5c370d503bcd32734b
KieuDucThinh <kdthinh04@gmail.com>
2025-05-20 17:01:13 +0700

    Update FoundTourList UI

--- js/search/filter-input.js
+++ js/search/filter-input.js
@@ -63,44 +63,47 @@ $(function () {
         // });
 
         function displayTourCard(tour) {
-            let card = $('<div class="card shadow-primary mb-4" style="height: 280px;"></div>');
+            let card = $('<div class="col-12 col-md-5 col-lg-5 col-xl-12 card shadow-primary mb-4 fix-height" style="min-height: 280px;"></div>');
 
-            // const placesToVisit = tour.tour.placesToVisit;
+                // const placesToVisit = tour.tour.placesToVisit;
 
-            let tourName = tour.tour.tourName;
-            if (tourName.length > 150)
-                tourName = tourName.substring(0, 140) + "...";
+                let tourName = tour.tour.tourName;
+                if (tourName.length > 150)
+                    tourName = tourName.substring(0, 140) + "...";
 
 
-            let placesToVisitSub = tour.tour.placesToVisit;
-            if (placesToVisitSub.length > 40)
-                placesToVisitSub = placesToVisitSub.substring(0, 40) + "...";
+                let placesToVisitSub = tour.tour.placesToVisit;
+                if (placesToVisitSub.length > 40)
+                    placesToVisitSub = placesToVisitSub.substring(0, 40) + "...";
 
-            let price = formatNumberWithDots(Math.round((tour.discount.discountUnit == "%") ?
-                tour.adultTourPrice * (1 - tour.discount.discountValue / 100)
-                :
-                tour.adultTourPrice - tour.discount.discountValue));
+                let price = formatNumberWithDots(Math.round((tour.discount.discountUnit == "%") ?
+                    tour.adultTourPrice * (1 - tour.discount.discountValue / 100)
+                    :
+                    tour.adultTourPrice - tour.discount.discountValue));
 
-            let adultTourPrice = formatNumberWithDots(Math.round(tour.adultTourPrice));
+                let adultTourPrice = formatNumberWithDots(Math.round(tour.adultTourPrice));
 
-            const htmlToInsert = `
-                <div class="d-flex h-100">
-                    <div class="packages-img rounded-0 img-filter h-100 w-30 img-lg-width">
-                        <img class="card-img-top object-fit-cover h-100" src="${tour.tour.firstImageUrl}"
+                tourName = splitString(tourName, 90);
+
+                //d-flex  h-100
+                const htmlToInsert = `
+                <div class="row">
+                    <div class="col-12 col-xl-3 packages-img rounded-0 img-filter h-lg-100 w-30 img-lg-width pe-0 ps-0">
+                        <img class="card-img-top object-fit-cover w-30" src="${tour.tour.firstImageUrl}"
                                             alt="Card image">
                                         <span class="d-block text-primary bg-white rounded-2 tag-img px-3 py-2">${tour.tour.category.categoryName}</span>
-                                    </div>
-                                    <div class="card-content my-2 mx-3 w-100">
+                    </div>
+                    <div class="col-12 col-xl-7 card-content px-3 mt-3 mt-lg-3 px-lg-2 my-lg-2 w-md-100">
                                         <h5 class="text-primary fw-bold text-justify py-0 my-0 tour-title-lite">${tourName}</h5>
-                                        <div class="d-flex justify-content-between py-0 my-2">
+                                        <div class="d-xl-flex justify-content-between py-0 my-2">
                                             <div class="d-flex">
                                                 <i class="bi bi-ticket-fill text-primary me-2"></i>
-                                                <p class="heading5 text-primary fw-bold py-0 my-0">${tour.tourUnitId}
+                                                <p class="fs-6 text-primary fw-bold py-0 my-0">${tour.tourUnitId}
                                                 </p>
                                             </div>
-                                            <div class="d-flex">
+                                            <div class="d-flex mt-2 mt-lg-0">
                                                 <i class="bi bi-life-preserver text-primary me-2"></i>
-                                                <p class="heading5 text-primary fw-bold py-0 my-0">${tour.festival.festivalName}</p>
+                                                <p class="fs-6 text-primary fw-bold py-0 my-0">${tour.festival.festivalName}</p>
                                             </div>
                                         </div>
                                         <div class="d-flex justify-content-between py-0 my-0">
@@ -109,7 +112,7 @@ $(function () {
                                         </div>
                                         <p class="mb-2 heading6 text-dark pt-0"><i class="bi bi-geo-alt-fill me-2"></i><span
                                                 class="text-dark">${tour.tour.departurePlace}</span></p>
-                                        <div class="d-flex justify-content-between">
+                                        <div class="d-xl-flex justify-content-between">
                                             <p class="mb-2 heading6 text-dark pt-0"><i
                                                     class="bi bi-flag-fill me-2"></i><span class="text-dark">${placesToVisitSub}</span></p>
                                             <p
@@ -120,31 +123,31 @@ $(function () {
                                             <p class="mb-2 heading6 text-dark pt-0"><i class="bi bi-amd me-2"></i>
                                                 <span class="text-primary span-bold">Còn ${tour.availableCapacity} chỗ</span>
                                             </p>
-                                            <p class="heading3 text-end text-primary">${price} đ</p>
+                                            <p class="heading-price text-end text-primary">${price} đ</p>
                                         </div>
                                     </div>
                                 </div>
             `;
 
-            card.html(htmlToInsert);
+                card.html(htmlToInsert);
 
-            card.on({
-                'click': function () {
-                    localStorage.setItem("foundtour", JSON.stringify(tour));
-                    window.location.href = "tourunit.html";
-                }
-            })
-            // card.addEventListener('click', () => {
-            //     // Khi bấm vào thì hiện chi tiết
-            //     detailTitle.textContent = tour.title;
-            //     detailPrice.textContent = `Giá: ${tour.price.toLocaleString()} VND`;
-            //     detailDesc.textContent = `Mô tả: ${tour.description}`;
-            //     detailFull.textContent = `Chi tiết: ${tour.details}`;
-            //     detailBox.style.display = 'block';
-            // });
-            $('#card-list').append(card);
-
-            // tourList.appendChild(card);
+                card.on({
+                    'click': function () {
+                        localStorage.setItem("foundtour", JSON.stringify(tour));
+                        window.location.href = "tourunit.html";
+                    }
+                })
+                // card.addEventListener('click', () => {
+                //     // Khi bấm vào thì hiện chi tiết
+                //     detailTitle.textContent = tour.title;
+                //     detailPrice.textContent = `Giá: ${tour.price.toLocaleString()} VND`;
+                //     detailDesc.textContent = `Mô tả: ${tour.description}`;
+                //     detailFull.textContent = `Chi tiết: ${tour.details}`;
+                //     detailBox.style.display = 'block';
+                // });
+                // let cardContainer = $('<div class="col-12 col-md-6 col-lg-12"></div>');
+                $('#card-list').append(card);
+                // tourList.appendChild(card);
         }
 
         function formatNumberWithDots(numberString) {
@@ -165,4 +168,10 @@ $(function () {
         }
     })
 
+    function splitString(s, len) {
+                if (s.length > len) {
+                    return s.substring(0, len) + '...'
+                }
+                return s;
+            }
 })
--- js/search/scroll.js
+++ js/search/scroll.js
@@ -22,9 +22,11 @@ $(function () {
         let temp = JSON.parse(sessionStorage.getItem('tourstemp'));
         console.log(temp);
         currentPage++;
-        for(let i=currentPage*pageSize; i<(currentPage+1)*pageSize; i++){
+        console.log('CP'+currentPage);
+
+        for (let i = currentPage * pageSize; i < (currentPage + 1) * pageSize; i++) {
             displayTourCard(temp[i]);
-            if(i==maxRs-1)
+            if (i == maxRs - 1)
                 return;
         }
 
@@ -32,9 +34,10 @@ $(function () {
     }
 
     function displayTourCard(tour) {
-        let card = $('<div class="card shadow-primary mb-4" style="height: 280px;"></div>');
+        let card = $('<div class="col-12 col-md-5 col-lg-5 col-xl-12 card shadow-primary mb-4 fix-height" style="min-height: 280px;"></div>');
 
         // const placesToVisit = tour.tour.placesToVisit;
+        if(tour==null) return;
 
         let tourName = tour.tour.tourName;
         if (tourName.length > 150)
@@ -52,24 +55,27 @@ $(function () {
 
         let adultTourPrice = formatNumberWithDots(Math.round(tour.adultTourPrice));
 
+        tourName = splitString(tourName, 90);
+
+        //d-flex  h-100
         const htmlToInsert = `
-                <div class="d-flex h-100">
-                    <div class="packages-img rounded-0 img-filter h-100 w-30 img-lg-width">
-                        <img class="card-img-top object-fit-cover h-100" src="${tour.tour.firstImageUrl}"
+                <div class="row">
+                    <div class="col-12 col-xl-3 packages-img rounded-0 img-filter h-lg-100 w-30 img-lg-width pe-0 ps-0">
+                        <img class="card-img-top object-fit-cover w-30" src="${tour.tour.firstImageUrl}"
                                             alt="Card image">
                                         <span class="d-block text-primary bg-white rounded-2 tag-img px-3 py-2">${tour.tour.category.categoryName}</span>
-                                    </div>
-                                    <div class="card-content my-2 mx-3 w-100">
+                    </div>
+                    <div class="col-12 col-xl-7 card-content px-3 mt-3 mt-lg-3 px-lg-2 my-lg-2 w-md-100">
                                         <h5 class="text-primary fw-bold text-justify py-0 my-0 tour-title-lite">${tourName}</h5>
-                                        <div class="d-flex justify-content-between py-0 my-2">
+                                        <div class="d-xl-flex justify-content-between py-0 my-2">
                                             <div class="d-flex">
                                                 <i class="bi bi-ticket-fill text-primary me-2"></i>
-                                                <p class="heading5 text-primary fw-bold py-0 my-0">${tour.tourUnitId}
+                                                <p class="fs-6 text-primary fw-bold py-0 my-0">${tour.tourUnitId}
                                                 </p>
                                             </div>
-                                            <div class="d-flex">
+                                            <div class="d-flex mt-2 mt-lg-0">
                                                 <i class="bi bi-life-preserver text-primary me-2"></i>
-                                                <p class="heading5 text-primary fw-bold py-0 my-0">${tour.festival.festivalName}</p>
+                                                <p class="fs-6 text-primary fw-bold py-0 my-0">${tour.festival.festivalName}</p>
                                             </div>
                                         </div>
                                         <div class="d-flex justify-content-between py-0 my-0">
@@ -78,7 +84,7 @@ $(function () {
                                         </div>
                                         <p class="mb-2 heading6 text-dark pt-0"><i class="bi bi-geo-alt-fill me-2"></i><span
                                                 class="text-dark">${tour.tour.departurePlace}</span></p>
-                                        <div class="d-flex justify-content-between">
+                                        <div class="d-xl-flex justify-content-between">
                                             <p class="mb-2 heading6 text-dark pt-0"><i
                                                     class="bi bi-flag-fill me-2"></i><span class="text-dark">${placesToVisitSub}</span></p>
                                             <p
@@ -89,7 +95,7 @@ $(function () {
                                             <p class="mb-2 heading6 text-dark pt-0"><i class="bi bi-amd me-2"></i>
                                                 <span class="text-primary span-bold">Còn ${tour.availableCapacity} chỗ</span>
                                             </p>
-                                            <p class="heading3 text-end text-primary">${price} đ</p>
+                                            <p class="heading-price text-end text-primary">${price} đ</p>
                                         </div>
                                     </div>
                                 </div>
@@ -100,8 +106,6 @@ $(function () {
         card.on({
             'click': function () {
                 localStorage.setItem("foundtour", JSON.stringify(tour));
-                if (sessionStorage.getItem("tourChoiced"))
-                    sessionStorage.removeItem("tourChoiced");
                 window.location.href = "tourunit.html";
             }
         })
@@ -113,8 +117,8 @@ $(function () {
         //     detailFull.textContent = `Chi tiết: ${tour.details}`;
         //     detailBox.style.display = 'block';
         // });
+        // let cardContainer = $('<div class="col-12 col-md-6 col-lg-12"></div>');
         $('#card-list').append(card);
-
         // tourList.appendChild(card);
     }
 
@@ -134,5 +138,16 @@ $(function () {
         // Sử dụng toLocaleString để định dạng với dấu chấm phân cách hàng nghìn
         return number.toLocaleString('vi-VN');
     }
+
+    function splitString(s, len) {
+        if (s.length > len) {
+            return s.substring(0, len) + '...'
+        }
+        return s;
+    }
+
+    $('#btnApply, #btnFind2, #btnFind, #btnBook, #bookNow').click(function () {
+        currentPage = 0;
+    })
 
 })
